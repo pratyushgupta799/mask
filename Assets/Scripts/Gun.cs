5a3792d300a2481601909ee0b6bf9e6fa11275c1
Pratyush-bruce-Gupta <pratyushgupta04@gmail.com>
2025-12-27 07:35:04 +0530

    double jump

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -11,6 +11,7 @@ public class Gun : MonoBehaviour
     [SerializeField] private TrailRenderer bulletTrail;
     [SerializeField] private float shootDelay = 0.1f;
     [SerializeField] private LayerMask mask;
+    [SerializeField] private Camera playerCamera;
 
     private float lastShootTime;
 
@@ -25,26 +26,36 @@ public class Gun : MonoBehaviour
 
     private void Shoot()
     {
-        if (lastShootTime + shootDelay < Time.time)
+        if (Time.time < lastShootTime + shootDelay) return;
+        lastShootTime = Time.time;
+
+        shootingSystem.Stop(true, ParticleSystemStopBehavior.StopEmittingAndClear);
+        shootingSystem.Play();
+
+        Ray camRay = playerCamera.ViewportPointToRay(new Vector3(0.5f, 0.5f));
+        Vector3 targetPoint;
+
+        if (Physics.Raycast(camRay, out RaycastHit camHit, 1000f, mask))
         {
-            shootingSystem.Play();
-            Vector3 direction = GetDirection();
-
-            if (Physics.Raycast(bulletSpawnPoint.position, direction, out RaycastHit hit, float.MaxValue, mask))
-            {
-                TrailRenderer trail = Instantiate(bulletTrail, bulletSpawnPoint.position, Quaternion.identity);
-
-                StartCoroutine(SpawnTrail(trail, hit));
-                
-                lastShootTime = Time.time;
-            }
+            TrailRenderer trail = Instantiate(bulletTrail, bulletSpawnPoint.position, Quaternion.identity);
+            
+            StartCoroutine(SpawnTrail(trail, camHit));
         }
+        else
+        {
+            targetPoint = camRay.origin + camRay.direction * 100f;
+            TrailRenderer trail = Instantiate(bulletTrail, bulletSpawnPoint.position, Quaternion.identity);
+            
+            StartCoroutine(SpawnTrail(trail, targetPoint));
+        }
+
+        
+
+        lastShootTime = Time.time;
     }
 
-    private Vector3 GetDirection()
+    private Vector3 GetDirection(Vector3 direction)
     {
-        Vector3 direction = bulletSpawnPoint.forward;
-
         if (addBulletSpread)
         {
             direction += new Vector3(
@@ -73,6 +84,23 @@ public class Gun : MonoBehaviour
         }
         trail.transform.position = hit.point;
         Instantiate(impactParticleSystem, hit.point, Quaternion.LookRotation(hit.normal));
+        
+        Destroy(trail.gameObject, trail.time);
+    }
+    
+    private IEnumerator SpawnTrail(TrailRenderer trail, Vector3 targetPoint)
+    {
+        float time = 0;
+        Vector3 startPosition = trail.transform.position;
+
+        while (time < 10)
+        {
+            trail.transform.position = Vector3.Lerp(startPosition, targetPoint, time);
+            time += Time.deltaTime / trail.time;
+            
+            yield return null;
+        }
+        trail.transform.position = targetPoint;
         
         Destroy(trail.gameObject, trail.time);
     }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,10 +2,18 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float moveSpeed = 5f;
-    public float mouseSensitivityHorizontal = 2f;
-    public float mouseSensitivityVertical = 2f;
-    public Transform cam;
+    [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private float mouseSensitivityHorizontal = 2f;
+    [SerializeField] private float mouseSensitivityVertical = 2f;
+    [SerializeField] private float jumpForce = 5f;
+    [SerializeField] private Transform cam;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private float groundDistance = 0.2f;
+    [SerializeField] private LayerMask groundMask;
+    
+    private bool isGrounded;
+    
+    private int jumpCount = 0;
 
     float xRotation = 0f;
     Rigidbody rb;
@@ -19,7 +27,33 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-        // Mouse look
+        CheckGrounded();
+        CamMovement();
+        PlayerMove();
+        PlayerJump();
+    }
+
+    private void CheckGrounded()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        if (isGrounded)
+        {
+            jumpCount = 0;
+        }
+    }
+
+    void PlayerMove()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+        rb.linearVelocity = new Vector3(move.x * moveSpeed * Time.deltaTime, rb.linearVelocity.y,
+            move.z * moveSpeed * Time.deltaTime);
+    }
+
+    void CamMovement()
+    {
         float mouseX = Input.GetAxis("Mouse X") * mouseSensitivityVertical * 100f * Time.deltaTime;
         float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivityHorizontal * 100f * Time.deltaTime;
 
@@ -30,13 +64,12 @@ public class PlayerController : MonoBehaviour
         transform.Rotate(Vector3.up * mouseX);
     }
 
-    void FixedUpdate()
+    void PlayerJump()
     {
-        // Movement
-        float x = Input.GetAxis("Horizontal");
-        float z = Input.GetAxis("Vertical");
-
-        Vector3 move = transform.right * x + transform.forward * z;
-        rb.linearVelocity = new Vector3(move.x * moveSpeed, rb.linearVelocity.y, move.z * moveSpeed);
+        if (Input.GetButtonDown("Jump") && (isGrounded || jumpCount < 1))
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            jumpCount++;
+        }
     }
 }
